5437da9ea6cb20d36190a3f0541e3cc1ca574264
aryavpal <aryavpal@gmail.com>
2017-02-06 09:16:37 -0500

    cleaned up attends

--- client/buildSessions/build_list.js
+++ client/buildSessions/build_list.js
@@ -3,19 +3,26 @@
  */
 Template.buildSessionList.helpers({
     buildSession: function() {
-        return BuildSessions.find();
+        return BuildSessions.find({}, {sort: {date: 1}});
     },
     comingnotcoming: function(sessionid){
-       console.log(_.contains(BuildSessions.findOne({_id: sessionid}).attend, Meteor.userId()));
        return _.contains(BuildSessions.findOne({_id: sessionid}).attend, Meteor.userId())?'not-coming':'coming';
 
-    }
+    },
+    team: function() {
+        return Teams;
+    },
+    queryuser: function(users, teamid) {
+        console.log(users);
+        console.log(teamid);
+        return Meteor.users.find({
+            _id: {$in: users},
+            profile: {team: teamid}
+        });
+    },
 });
 
 Template.buildSessionList.events({
- //    if(session.attend.includes(e.target.Id) e.gettext == add){
- //  //  if(check(session.attend), Meteor.userId == true){
-
     'click .not-coming': function(e) {
         e.preventDefault();
         BuildSessions.update({_id: e.target.id}, {$pull: {attend: Meteor.userId()}});
@@ -23,8 +30,6 @@ Template.buildSessionList.events({
 
     'click .coming': function (e) {
         e.preventDefault();
-      //  n = BuildSessions.findOne(e.target.id);
         BuildSessions.update({_id: e.target.id}, {$push: {attend: Meteor.userId()}});
-
-}
+    },
 });
