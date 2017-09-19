e5fc01fb8ca29a505afc0216bcdf0b06a71ebcc2
cforster <cf@stewert.nyc>
2017-09-18 22:02:34 -0400

    protect users collection with better publication

--- client/buildSessions/build_list.js
+++ client/buildSessions/build_list.js
@@ -64,7 +64,7 @@ Template.buildSessionList.helpers({
                     doc.isAbsent = BuildSessions.find({_id: sessionid, absent: doc._id}).count()>0
                     doc.absent = doc.isAbsent?"absent":"";
                     doc.sessionid = sessionid;
-                    if(!Meteor.userId()) doc.username = doc.username.replace(/[\w-]/g, 'x'); //anon if not logged in
+                    // if(!Meteor.userId()) doc.username = doc.username.replace(/[\w-]/g, 'x'); //anon if not logged in
                 return doc;
                 }
             }
@@ -82,11 +82,14 @@ Template.buildSessionList.helpers({
     eating: function(session) {
         return Meteor.users.find({
             _id: {$in: session.food}
-        }, {
-            transform: function (doc) {
-                if(!Meteor.userId()) doc.username = doc.username.replace(/[\w-]/g, 'x'); //anon if not logged in
-                return doc;
-            }});
+        }
+        // , {
+            // transform: function (doc) {
+            //     if(!Meteor.userId()) doc.username = doc.username.replace(/[\w-]/g, 'x'); //anon if not logged in
+            //     return doc;
+            // }
+        // }
+        );
     },
     lockMoment: function() {
         l = moment(this.start).subtract(this.locktime, 'hours')
--- server/publications.js
+++ server/publications.js
@@ -8,6 +8,24 @@ Meteor.publish('buildSessions', function(){
 Meteor.publish(null, function (){
     return Meteor.roles.find();
 });
-Meteor.publish(null, function() {
-    return Meteor.users.find();
+
+
+Meteor.publishTransformed(null, function() {
+    let cursor = Meteor.users.find({}, {
+        fields: {
+            username: 1,
+            profile: 1
+        }
+    });
+
+    //if logged in, make the username x-ed out
+    if(!this.userId) {
+        cursor.serverTransform(function(doc) {
+            doc.username = doc.username.replace(/[\w-]/g, 'x');
+            return doc;
+        });
+    }
+    return cursor;
+
 });
+
