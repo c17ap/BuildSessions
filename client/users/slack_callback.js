1133e53449c9afc0a1ae4f33363f00903b79611d
William B <william@barkoffusa.com>
2019-03-01 17:26:26 -0500

    Fix callback bug

--- client/users/slack_callback.js
+++ client/users/slack_callback.js
@@ -3,6 +3,9 @@ Template.slackCallback.rendered = function () {
 	var code = urlParams.get("code")
 	Meteor.call('connectSlack', code, function (err, resp) {
 		if (err) console.error(err)
-		Router.go('profile')
+		$(".connecting").fadeOut(function () {
+			$(".done-connecting").show()
+
+		})
 	})
 };
